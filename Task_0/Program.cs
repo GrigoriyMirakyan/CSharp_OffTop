1625152dd7d678db89998902ffa87dcf0736f6b0
Grigoriy Mirakyan <grigoriy66660@gmail.com>
2022-04-28 20:16:13 +0300

    Task_0

---
+++ Task_0/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число -> ");
+int num = int.Parse(Console.ReadLine());
+int result = num * num;
+Console.Write("Квадрат числа равен -> ");
+Console.WriteLine(result);
+Console.ReadKey();
